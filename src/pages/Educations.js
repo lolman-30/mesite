33090537e5664d2fd853ffc460ea6e8f62e6ad23
bagas <bagasdwipranata@gmail.com>
2023-10-21 19:12:01 +0700

    add: CV Update

--- src/pages/Educations.js
+++ src/pages/Educations.js
@@ -1,22 +1,9 @@
 import React from "react";
-import {
-  Box,
-  Container,
-  Flex,
-  Heading,
-  Stack,
-  Text,
-} from "@chakra-ui/react";
+import { Box, Container, Flex, Heading, Stack, Text } from "@chakra-ui/react";
 
 const Card = ({ heading, description }) => {
   return (
-    <Box
-      flex="1"
-      borderWidth="1px"
-      borderRadius="lg"
-      overflow="hidden"
-      p={5}
-    >
+    <Box flex="1" borderWidth="1px" borderRadius="lg" overflow="hidden" p={5}>
       <Stack spacing={2}>
         <Heading size="md">{heading}</Heading>
         <Text mt={1} fontSize={"sm"}>
@@ -35,8 +22,8 @@ const App = () => {
           Pendidikan
         </Heading>
         <Text color={"gray.600"} fontSize={{ base: "sm", sm: "lg" }}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis obcaecati ut
-          cupiditate pariatur, dignissimos, placeat amet officiis.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+          obcaecati ut cupiditate pariatur, dignissimos, placeat amet officiis.
         </Text>
       </Stack>
 
@@ -44,16 +31,15 @@ const App = () => {
         <Flex flexWrap="wrap" gridGap={7} justify="center">
           <Card
             heading={"Universitas Nusantara PGRI Kediri"}
-            description={"Teknik Informatika Bulan 2019 - Agustus 2023 IPK : 3.58"}
+            description={
+              "Teknik Informatika Agustus 2019 - Agustus 2023 IPK : 3.58"
+            }
           />
           <Card
             heading={"SMK PGRI 3 Nganjuk"}
             description={"Teknik Komputer dan Jaringan Bulan Tahun"}
           />
-          <Card
-            heading={"SMP Negeri 7 Nganjuk"}
-            description={"Bulan Tahun"}
-          />
+          <Card heading={"SMP Negeri 7 Nganjuk"} description={"Bulan Tahun"} />
           <Card
             heading={"SD Negeri Ganung Kidul 1 Nganjuk"}
             description={"Bulan Tahun"}
